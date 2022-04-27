027aae33f982e15cbb1e92dc998e6c4f38e1a403
jasper <s218005@students.aie.edu>
2022-04-27 16:58:09 -0500

    made a light and now the player can move just have to make input
    started the needed test files

--- Assets/Skirps/Movement/MovementBehaviour1.cs
+++ Assets/Skirps/Movement/MovementBehaviour1.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementBehaviour : MonoBehaviour
+public class MovementBehaviour1 : MonoBehaviour
 {
     [SerializeField]
     private Vector3 _velocity;
 
-    public Vector3 Veclicty 
+    public Vector3 Velocity 
     {
         get {return _velocity; }
         set {value = _velocity; }
     }
-    // Update is called once per frame
 
+
+    // Update is called once per frame
     void Update()
     {
         transform.position += _velocity * Time.deltaTime;
--- Assets/Skirps/Movement/PlayerMovement.cs
+++ Assets/Skirps/Movement/PlayerMovement.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MovementBehaviour1
 {
+    [SerializeField]
+    private float _speed;
+    private Rigidbody _rigidbody;
+    private Vector2 _moveDirection;
+
+    Vector2 MoveDirection 
+    {
+        get {return _moveDirection; }
+        set {value = _moveDirection; }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 velocity = MoveDirection * _speed * Time.deltaTime;
+        _rigidbody.MovePosition(transform.position + velocity);
     }
 }
